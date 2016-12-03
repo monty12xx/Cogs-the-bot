92605dc362713336681faf0fe1b80caadefe273c
monty12xx <montywooz@gmail.com>
2016-12-03 10:48:32 +0000

    FIXED ALL ERORRS

--- move/move.py
+++ move/move.py
@@ -17,11 +17,12 @@ class move:
         for user in users:
             try:
                 await self.bot.move_member(user, channel)
-                await self.bot.say("Moving **{0}** to **__{1}__**".format(user, channel))
+                await self.bot.say("Moved **{0}** to **__{1}__**".format(user, channel))
                 await asyncio.sleep(1)
                 await self.bot.say("**done**")
             except discord.Forbidden:
                await self.bot.say("I need Moving Perms to be able to move peeps")
+                return
             if user.voice_channel is None:
                 await self.bot.say('{} is not in a voice channel '.format(user.name))
                 return

9bce8d90ae9f2967a195bc6de4ec16cd5c74d946
monty12xx <montywooz@gmail.com>
2016-12-03 16:37:47 +0000

    Update move.py

--- move/move.py
+++ move/move.py
@@ -16,16 +16,16 @@ class move:
         """move a user to another voice channel
 		% "channel" @user ,you can move more than a one user or a bunch of users"""
         s = ","
-
-        try:
-            await self.bot.move_member(user, channel)
-            seq = self.bot.say("Moved **{0}** to **__{1}__**".format(user, channel))
-            await self.bot.say(s.join(seq))
-            await asyncio.sleep(1)
-            await self.bot.say("**Done :thumbup::skin-tone-2:**")
-        except discord.Forbidden:
-            await self.bot.say("I need the moving perms")
-        return
+        for user in users:    
+            try:
+                await self.bot.move_member(user, channel)
+                seq = self.bot.say("Moved **{0}** to **__{1}__**".format(user, channel))
+                await self.bot.say(s.join(seq))
+                await asyncio.sleep(1)
+                await self.bot.say("**Done :thumbup::skin-tone-2:**")
+            except discord.Forbidden:
+                await self.bot.say("I need the moving perms")
+            return
 
 
 def setup(bot):

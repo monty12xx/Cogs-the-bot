2ec98183489365dbad2afd4e9b980a2dfad30191
monty12xx <montywooz@gmail.com>
2016-12-06 07:18:34 +0000

    Update pogo.py

--- pogo/pogo.py
+++ pogo/pogo.py
@@ -5,6 +5,58 @@ from discord.ext import commands
 class pogo:
     def __init__(self, bot):
         self.bot = bot
+        self._announce_msg = None
+        self._settings = dataIO.load_json('data/admin/settings.json')
+        self._settable_roles = self._settings.get("ROLES", {})
+
+    async def _confirm_invite(self, server, owner, ctx):
+        answers = ("yes", "y")
+        invite = await self.bot.create_invite(server)
+        if ctx.message.channel.is_private:
+            await self.bot.say(invite)
+        else:
+            await self.bot.say("Are you sure you want to post an invite to {} "
+                               "here? (yes/no)".format(server.name))
+            msg = await self.bot.wait_for_message(author=owner, timeout=15)
+            if msg is None:
+                await self.bot.say("I guess not.")
+            elif msg.content.lower().strip() in answers:
+                await self.bot.say(invite)
+            else:
+                await self.bot.say("Alright then.")
+
+    def _get_selfrole_names(self, server):
+        if server.id not in self._settable_roles:
+            return None
+        else:
+            return self._settable_roles[server.id]
+
+    def _is_server_locked(self):
+        return self._settings.get("SERVER_LOCK", False)
+
+    def _role_from_string(self, server, rolename, roles=None):
+        if roles is None:
+            roles = server.roles
+        role = discord.utils.find(lambda r: r.name.lower() == rolename.lower(),
+                                  roles)
+        try:
+            log.debug("Role {} found from rolename {}".format(
+                role.name, rolename))
+        except:
+            log.debug("Role not found for rolename {}".format(rolename))
+        return role
+
+    def _save_settings(self):
+        dataIO.save_json('data/admin/settings.json', self._settings)
+
+    def _set_selfroles(self, server, rolelist):
+        self._settable_roles[server.id] = rolelist
+        self._settings["ROLES"] = self._settable_roles
+        self._save_settings()
+
+    def _set_serverlock(self, lock=True):
+        self._settings["SERVER_LOCK"] = lock
+        self._save_settings()
 
     @commands.command(no_pm=True, pass_context=True)
     async def pogo(self, ctx, rolename, user: discord.Member=None):

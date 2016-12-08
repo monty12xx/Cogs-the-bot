b9ddca28929d7892d0d4f51c7f9dffb2cd51d48d
monty12xx <montywooz@gmail.com>
2016-12-08 15:49:57 +0000

    Update sinfo.py

--- sinfo/sinfo.py
+++ sinfo/sinfo.py
@@ -1,149 +1,51 @@
-from discord.ext import commands
-from datetime import datetime
+from .utils import utils
+import traceback
+import datetime
+import asyncio
 import discord
-from .utils import checks
-import aiohttp
-from urllib.parse import parse_qs
-from lxml import etree
 
-def date(argument):
-    formats = (
-        '%Y/%m/%d',
-        '%Y-%m-%d',
-    )
-
-    for fmt in formats:
-        try:
-            return datetime.strptime(argument, fmt)
-        except ValueError:
-            continue
-
-    raise commands.BadArgument('Cannot convert to date. Expected YYYY/MM/DD or YYYY-MM-DD.')
-
-class Buttons:
-    """Buttons that make you feel."""
-
-    def __init__(self, bot):
+class Welcome(): #Allow to welcome new members who join server. If it enable, will send them a message.
+    def __init__(self,bot):
         self.bot = bot
+        self.redis = bot.db.redis
 
-    @commands.command(hidden=True)
-    async def feelgood(self):
-        """press"""
-        await self.bot.say('*pressed*')
-
-    @commands.command(hidden=True)
-    async def feelbad(self):
-        """depress"""
-        await self.bot.say('*depressed*')
-
-    @commands.command()
-    async def love(self):
-        """What is love?"""
-        await self.bot.say('http://i.imgur.com/JthwtGA.png')
-
-    @commands.command(hidden=True)
-    async def bored(self):
-        """boredom looms"""
-        await self.bot.say('http://i.imgur.com/BuTKSzf.png')
-
-    @commands.command(pass_context=True)
-    @checks.mod_or_permissions(manage_messages=True)
-    async def nostalgia(self, ctx, date: date, *, channel: discord.Channel = None):
-        """Pins an old message from a specific date.
-        If a channel is not given, then pins from the channel the
-        command was ran on.
-        The format of the date must be either YYYY-MM-DD or YYYY/MM/DD.
-        """
-
-        if channel is None:
-            channel = ctx.message.channel
-
-        async for m in self.bot.logs_from(channel, after=date, limit=1):
+    async def on_member_join(self,member):
+        if await self.redis.hget("{}:Config:Cogs".format(member.server.id),"welcome") == "on":
+            config = await self.redis.hgetall("{}:Welcome:Message".format(member.server.id))
             try:
-                await self.bot.pin_message(m)
-            except:
-                await self.bot.say('\N{THUMBS DOWN SIGN} Could not pin message.')
-            else:
-                await self.bot.say('\N{THUMBS UP SIGN} Successfully pinned message.')
+                if config.get("enable_message") == "on":
+                    if config.get("whisper") == "on":
+                        msg = await self.bot.send_message(member,config["message"].format(user=member.name,server=member.server,user_mention=member.mention))
+                    else:
+                        msg =await self.bot.send_message(self.bot.get_channel(config["channel"]),config["message"].format(user=member.name,server=member.server,user_mention=member.mention))
+                    if config.get("enable_delete") == "on":
+                            await asyncio.sleep(int(config["delete_msg"]))
+                            await self.bot.delete_message(msg)
+            except Exception as e:
+                await self.bot.send_message(member.server.owner,"There is an error with a newcomer, please report this to the creator.\n {}".format(e))
+                Current_Time = datetime.datetime.utcnow().strftime("%b/%d/%Y %H:%M:%S UTC")
+                utils.prRed(Current_Time)
+                utils.prRed("Error!")
+                utils.prRed(traceback.format_exc())
+                error =  '```py\n{}\n```'.format(traceback.format_exc())
+                await self.bot.send_message(self.bot.get_channel("123934679618289669"), "```py\n{}```".format(Current_Time + "\n"+ "ERROR!") + "\n" +  error)
 
-    @nostalgia.error
-    async def nostalgia_error(self, error, ctx):
-        if type(error) is commands.BadArgument:
-            await self.bot.say(error)
-
-    async def get_google_entries(self, query):
-        params = {
-            'q': query,
-            'safe': 'on'
-        }
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)'
-        }
-
-        # list of URLs
-        entries = []
-
-        async with aiohttp.get('https://www.google.com/search', params=params, headers=headers) as resp:
-            if resp.status != 200:
-                raise RuntimeError('Google somehow failed to respond.')
-
-            root = etree.fromstring(await resp.text(), etree.HTMLParser())
-
-            """
-            Tree looks like this.. sort of..
-            <div class="g">
-                ...
-                <h3>
-                    <a href="/url?q=<url>" ...>title</a>
-                </h3>
-                ...
-                <span class="st">
-                    <span class="f">date here</span>
-                    summary here, can contain <em>tag</em>
-                </span>
-            </div>
-            """
-
-            search_nodes = root.findall(".//div[@class='g']")
-            for node in search_nodes:
-                url_node = node.find('.//h3/a')
-                if url_node is None:
-                    continue
-
-                url = url_node.attrib['href']
-                if not url.startswith('/url?'):
-                    continue
-
-                url = parse_qs(url[5:])['q'][0] # get the URL from ?q query string
-
-                # if I ever cared about the description, this is how
-                entries.append(url)
-
-                # short = node.find(".//span[@class='st']")
-                # if short is None:
-                #     entries.append((url, ''))
-                # else:
-                #     text = ''.join(short.itertext())
-                #     entries.append((url, text.replace('...', '')))
-
-        return entries
-
-    @commands.command(aliases=['go'])
-    async def g(self, *, query):
-        """Searches google and gives you top result."""
-        try:
-            entries = await self.get_google_entries(query)
-        except RuntimeError as e:
-            await self.bot.say(str(e))
-        else:
-            next_two = entries[1:3]
-            if next_two:
-                formatted = '\n'.join(map(lambda x: '<%s>' % x, next_two))
-                msg = '{}\n\n**See also:**\n{}'.format(entries[0], formatted)
-            else:
-                msg = entries[0]
-
-            await self.bot.say(msg)
+        #Now assign a roles.
+            if config.get("role") == "on":
+                try:
+                    role_list = await self.redis.smembers('{}:Welcome:Assign_Roles'.format(member.server.id))
+                    role_obj=[]
+                    for x in role_list:
+                        role_obj.append(discord.utils.get(member.server.roles,id=x))
+                    await self.bot.add_roles(member,*role_obj)
+                except Exception as e:
+                    await self.bot.send_message(member.server.owner,"There is an error with a newcomer's roles, please report this to the creator.\n {}".format(e))
+                    Current_Time = datetime.datetime.utcnow().strftime("%b/%d/%Y %H:%M:%S UTC")
+                    utils.prRed(Current_Time)
+                    utils.prRed("Error!")
+                    utils.prRed(traceback.format_exc())
+                    error =  '```py\n{}\n```'.format(traceback.format_exc())
+                    await self.bot.send_message(self.bot.get_channel("123934679618289669"), "```py\n{}```".format(Current_Time + "\n"+ "ERROR!") + "\n" +  error)
 
 def setup(bot):
-    bot.add_cog(Buttons(bot))
+    bot.add_cog(Welcome(bot))

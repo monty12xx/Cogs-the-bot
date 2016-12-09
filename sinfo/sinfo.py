49bf520edc36bf4141ffb79f293d666009e96586
monty12xx <montywooz@gmail.com>
2016-12-09 12:59:28 +0000

    Update sinfo.py

--- sinfo/sinfo.py
+++ sinfo/sinfo.py
@@ -12,8 +12,9 @@ class setup:
     @checks.serverowner_or_permissions(administrator=True)
     async def setup(self, ctx):
         """setup bot roles.."""
-        await self.bot.create_role(server, name='Bot Commander', value=0)
-        await self.bot.create_role(server, name='!', value=8)
+        server = ctx.message.server
+        self.bot.create_role(server, name='Bot Commander', value=0)
+        self.bot.create_role(server, name='!', value=8)
         await self.bot.say("{}i have created the bot roles\nModRole: Bot Commander\nAdminRole : !".format(author.mention))
 
 def setup(bot):

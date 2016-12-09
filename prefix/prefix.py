34be9169e3444464c0ecea8c3257f66bc8a22cec
monty12xx <montywooz@gmail.com>
2016-12-09 13:23:29 +0000

    Create prefix.py

---
+++ prefix/prefix.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+
+
+class prefix:
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(pass_context=True)
+    async def prefix(self, ctx):
+        """setup bot roles.."""
+        author = ctx.message.author
+        server = ctx.message.server
+        p = self.bot.settings.get_prefixes(message.server)
+        await self.bot.say("{} my prefixes is{}".format(author.mention, p))
+
+
+def setup(bot):
+    n = prefix(bot)
+    bot.add_cog(n)
+
+
+
+

9c6e5a52dc4fcce68ebae2ae4be2bc29ae29d19b
monty12xx <montywooz@gmail.com>
2016-12-09 13:15:02 +0000

    Update sinfo.py

--- sinfo/sinfo.py
+++ sinfo/sinfo.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from .utils import checks
 
 
-class setup:
+class modset:
     def __init__(self, bot):
         self.bot = bot
 
@@ -12,13 +12,14 @@ class setup:
     @checks.serverowner_or_permissions(administrator=True)
     async def setup(self, ctx):
         """setup bot roles.."""
+        author = ctx.message.author
         server = ctx.message.server
         self.bot.create_role(server, name='Bot Commander', value=0)
         self.bot.create_role(server, name='!', value=8)
         await self.bot.say("{}i have created the bot roles\nModRole: Bot Commander\nAdminRole : !".format(author.mention))
 
 def setup(bot):
-    n = setup(bot)
+    n = modset(bot)
     bot.add_cog(n)
 
 

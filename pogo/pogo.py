ff89904585a29b4fef5d5099915d8f27801917b6
monty12xx <montywooz@gmail.com>
2016-12-10 13:12:46 +0000

    Update pogo.py

--- pogo/pogo.py
+++ pogo/pogo.py
@@ -1,103 +1,46 @@
-import discord
 from discord.ext import commands
+from random import choice, randint
+import requests
+import json
 import discord
-from discord.ext import commands
-from cogs.utils import checks
-from cogs.utils.dataIO import dataIO
-from __main__ import settings, send_cmd_help
-from copy import deepcopy
-import asyncio
-import logging
-import os
+class youtube:
+    """youtube"""
 
-log = logging.getLogger("red.admin")
-
-class pogo:
     def __init__(self, bot):
         self.bot = bot
-        self._announce_msg = None
-        self._settings = dataIO.load_json('data/admin/settings.json')
-        self._settable_roles = self._settings.get("ROLES", {})
 
-    async def _confirm_invite(self, server, owner, ctx):
-        answers = ("yes", "y")
-        invite = await self.bot.create_invite(server)
-        if ctx.message.channel.is_private:
-            await self.bot.say(invite)
-        else:
-            await self.bot.say("Are you sure you want to post an invite to {} "
-                               "here? (yes/no)".format(server.name))
-            msg = await self.bot.wait_for_message(author=owner, timeout=15)
-            if msg is None:
-                await self.bot.say("I guess not.")
-            elif msg.content.lower().strip() in answers:
-                await self.bot.say(invite)
-            else:
-                await self.bot.say("Alright then.")
-
-    def _get_selfrole_names(self, server):
-        if server.id not in self._settable_roles:
-            return None
-        else:
-            return self._settable_roles[server.id]
-
-    def _is_server_locked(self):
-        return self._settings.get("SERVER_LOCK", False)
-
-    def _role_from_string(self, server, rolename, roles=None):
-        if roles is None:
-            roles = server.roles
-        role = discord.utils.find(lambda r: r.name.lower() == rolename.lower(),
-                                  roles)
+    @commands.command(pass_context = True)
+    async def youtube(self, ctx, channelname):
+        payload = {"q" : channelname, "key" : "AIzaSyCWHEWGj6JS6l8tJs94TG3QWB_gM_1dCEM", "part" : "snippet", "type" : "channel", "maxResults" : 1}
+        r = requests.get("https://www.googleapis.com/youtube/v3/search", params = payload)
+        idjson = r.content
+        decoded1 = idjson.decode("utf-8")
+        jsonloaded1 = json.loads(decoded1)
         try:
-            log.debug("Role {} found from rolename {}".format(
-                role.name, rolename))
-        except:
-            log.debug("Role not found for rolename {}".format(rolename))
-        return role
+            channelid = (jsonloaded1["items"][0]['id']['channelId'])
+            thumbnail = (jsonloaded1['items'][0]['snippet']['thumbnails']['default']['url'])
+            channeln = (jsonloaded1['items'][0]['snippet']['title'])
+            payload = {"part" : "statistics",  "id" : channelid, "key" : "AIzaSyCWHEWGj6JS6l8tJs94TG3QWB_gM_1dCEM"}
+            b = requests.get("https://www.googleapis.com/youtube/v3/channels", params = payload)
+            stats = b.content
+            decoded2 = stats.decode("utf-8")
+            jsonloaded2 = json.loads(decoded2)
+            subscount = jsonloaded2["items"][0]['statistics']['subscriberCount']
+            viewcount = jsonloaded2["items"][0]['statistics']['viewCount']
+            videocount = jsonloaded2["items"][0]['statistics']['videoCount']
+            colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+            colour = int(colour, 16)
+            em = discord.Embed(colour=discord.Colour(value=colour))
+            em.add_field(name="Views:", value=viewcount)
+            em.add_field(name="Subscribers:", value=subscount)
+            em.add_field(name="Videos:", value=videocount)
+            em.set_author(name=channeln + ":", url=thumbnail, icon_url=thumbnail)
+            em.set_thumbnail(url=thumbnail)
+            await self.bot.say(embed=em)
+        except IndexError:
+            await self.bot.say("No such youtuber found")
 
-    def _save_settings(self):
-        dataIO.save_json('data/admin/settings.json', self._settings)
-
-    def _set_selfroles(self, server, rolelist):
-        self._settable_roles[server.id] = rolelist
-        self._settings["ROLES"] = self._settable_roles
-        self._save_settings()
-
-    def _set_serverlock(self, lock=True):
-        self._settings["SERVER_LOCK"] = lock
-        self._save_settings()
-
-    @commands.command(no_pm=True, pass_context=True)
-    async def pogo(self, ctx, rolename, user: discord.Member=None):
-        """Adds a role to a user, defaults to author
-
-        Role name must be in quotes if there are spaces."""
-        author = ctx.message.author
-        channel = ctx.message.channel
-        server = ctx.message.server
-
-        if user is None:
-            user = author
-
-        role = self._role_from_string(server, rolename)
-
-        if role is None:
-            await self.bot.say('That role cannot be found.')
-            return
-
-        if not channel.permissions_for(server.me).manage_roles:
-            await self.bot.say('I don\'t have manage_roles.')
-            return
-
-        if author.id == settings.owner:
-            pass
-        elif not channel.permissions_for(author).manage_roles:
-            raise commands.CheckFailure
-
-        await self.bot.add_roles(user, role)
-        await self.bot.say('Added user to team {} to {}'.format(role.name, user.name))
 
 def setup(bot):
-    n = pogo(bot)
+    n = youtube(bot)
     bot.add_cog(n)

c62a7efb38296128b87dbca48afb55ec61de9b33
monty12xx <montywooz@gmail.com>
2016-12-16 18:30:19 +0000

    Update help.py

--- help/help.py
+++ help/help.py
@@ -14,7 +14,7 @@ class help:
         server = ctx.message.server
         author = ctx.message.author
         await self.bot.say("{}check your dm ;)".format(author.mention))
-        await self.bot.whisper(author, ("You can find all my commands here:http://pastebin.com/dfyeK0mp\nIf you find a bug glitch or have any idea you can jion my support server: https://discord.gg/DmR6X\nif you want to invite me to yourserver Type %invite <3 enjoy."))
+        await self.bot.whisper("You can find all my commands here:http://pastebin.com/dfyeK0mp\nIf you find a bug glitch or have any idea you can jion my support server: https://discord.gg/DmR6X\nif you want to invite me to yourserver Type %invite <3 enjoy.")
 
 def setup(bot):
     n = help(bot)

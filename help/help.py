88483547eb0414c53b88734d23c021d1613d90a1
monty12xx <montywooz@gmail.com>
2016-12-18 15:13:52 +0000

    Update help.py

--- help/help.py
+++ help/help.py
@@ -14,7 +14,7 @@ class help:
         server = ctx.message.server
         author = ctx.message.author
         await self.bot.say(":mega: {}check your dm :ballot_box_with_check: ".format(author.mention))
-        await self.bot.whisper("You can find all my commands here: http://pastebin.com/dfyeK0mp\nIf you find a bug glitch or have any idea you can jion my support server: https://discord.gg/DmR6X\nif you want to invite me to yourserver Type %invite <3 enjoy.")
+        await self.bot.whisper("You can find all my commands here: http://pastebin.com/dfyeK0mp\nIf you find a bug glitch or have any idea you can jion my support server:https://discord.gg/8ftzUNw\nif you want to invite me to yourserver Type %invite <3 enjoy.")
 
 def setup(bot):
     n = help(bot)

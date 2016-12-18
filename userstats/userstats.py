6d8ecb1600b22a5c600f7790ab69f4425a19a051
monty12xx <montywooz@gmail.com>
2016-12-18 16:03:35 +0000

    Update userstats.py

--- userstats/userstats.py
+++ userstats/userstats.py
@@ -15,17 +15,14 @@ class userstats:
 
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         colour = int(colour, 16)
-        Online = "<:vpOnline:212789758110334977> Members: {}\n".format(
-            len([e for e in self.bot.get_all_members() if e.status == discord.Status.online]))
-        offline = "<:vpOffline:212790005943369728> Members: {}\n".format(
-            len([e for e in self.bot.get_all_members() if e.status == discord.Status.offline]))
-        others = "<:vpDnD:236744731088912384> | <:vpAway:212789859071426561> Members: {}\n".format(
-            len([e for e in self.bot.get_all_members() if e.status in [discord.Status.idle, discord.Status.dnd]]))
+        Online = len([e for e in self.bot.get_all_members() if e.status == discord.Status.online])
+        offline = len([e for e in self.bot.get_all_members() if e.status == discord.Status.offline])
+        others = len([e for e in self.bot.get_all_members() if e.status in [discord.Status.idle, discord.Status.dnd]])
         em = discord.Embed(colour=discord.Colour(value=colour))
         em.set_author(name="I am in **{}** Servers\n".format(len(self.bot.servers)), icon_url=author.avatar)
-        em.add_field(name="Online users:", value=Online)
-        em.add_field(name="Offline users:", value=offline)
-        em.add_field(name="Other users:", value=others)
+        em.add_field(name="<:vpOnline:212789758110334977> Members:", value=Online)
+        em.add_field(name="<:vpOffline:212790005943369728> Members: ", value=offline)
+        em.add_field(name="<:vpDnD:236744731088912384> | <:vpAway:212789859071426561> Members: ", value=others)
         await self.bot.say(embed=em)
 
 def setup(bot):

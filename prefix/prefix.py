8ce7ef474c0915d7a2b3a6a3cb64edfd1ccffe83
monty12xx <montywooz@gmail.com>
2016-12-23 21:27:27 +0000

    Update prefix.py

--- prefix/prefix.py
+++ prefix/prefix.py
@@ -1,26 +1,47 @@
 import discord
 from discord.ext import commands
-from .utils import checks
 
-
-class prefix:
+class party:
     def __init__(self, bot):
         self.bot = bot
-
-
+        
     @commands.command(pass_context=True)
-    async def prefix(self, ctx):
-        """setup bot roles.."""
-        author = ctx.message.author
-        server = ctx.message.server
-        p = ("%")
-        await self.bot.say("{} my prefixes is{}".format(author.mention, p))
-
-
+    @checks.is_owner()
+    async def partycrash(self, ctx, idnum=None):
+        """Lists servers and generates invites for them"""
+        owner = ctx.message.author
+        if idnum:
+            server = discord.utils.get(self.bot.servers, id=idnum)
+            if server:
+                await self._confirm_invite(server, owner, ctx)
+            else:
+                await self.bot.say("I'm not in that server")
+        else:
+            msg = ""
+            servers = sorted(self.bot.servers, key=lambda s: s.name)
+            for i, server in enumerate(servers, 1):
+                msg += "{}: {}\n".format(i, server.name)
+            msg += "\nTo post an invite for a server just type its number."
+            for page in pagify(msg, delims=["\n"]):
+                await self.bot.say(box(page))
+                await asyncio.sleep(1.5)  # Just in case for rate limits
+            msg = await self.bot.wait_for_message(author=owner, timeout=15)
+            if msg is not None:
+                try:
+                    msg = int(msg.content.strip())
+                    server = servers[msg - 1]
+                except ValueError:
+                    await self.bot.say("You must enter a number.")
+                except IndexError:
+                    await self.bot.say("Index out of range.")
+                else:
+                    try:
+                        await self._confirm_invite(server, owner, ctx)
+                    except discord.Forbidden:
+                        await self.bot.say("I'm not allowed to make an invite"
+                                           " for {}".format(server.name))
+            else:
+                await self.bot.say("Response timed out.")
 def setup(bot):
-    n = prefix(bot)
+    n = party(bot)
     bot.add_cog(n)
-
-
-
-

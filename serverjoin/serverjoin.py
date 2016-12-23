466a065e563aefbeb2da3e4e3570990a11d61bc6
monty12xx <montywooz@gmail.com>
2016-12-23 05:42:49 +0000

    Update serverjoin.py

--- serverjoin/serverjoin.py
+++ serverjoin/serverjoin.py
@@ -4,8 +4,9 @@ from discord.ext import commands
 class Shit:
     def __init__(self, bot):
         self.bot = bot
-    async def on_server_join(server):
-        await self.bot.send_message(server, "Thanks for adding me to your server , do %help to see all my commands <3")
+    async def on_server_join(self, server):
+        msg = "Thanks for adding me to your server , do %help to see all my commands <3"
+        await self.bot.send_message(server, msg)
 
 def setup(bot):
     n = Shit(bot)

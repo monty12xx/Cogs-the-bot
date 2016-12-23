16915acfe3c1f79fd6f253fbd15ec2d024dce72f
monty12xx <montywooz@gmail.com>
2016-12-23 05:46:35 +0000

    Update serverjoin.py

--- serverjoin/serverjoin.py
+++ serverjoin/serverjoin.py
@@ -5,7 +5,7 @@ class Shit:
     def __init__(self, bot):
         self.bot = bot
     async def on_server_join(self, server):
-        msg = "Thanks for adding me to your server , do %help to see all my commands <3"
+        msg = "Thanks for adding me to your server , do %help to see all my commands\nall my commands start with `%`\n if there is anything wrong with the bot you always do `%contact (whatever is wrong)` that will send me a message :) and ill answer when ever not busy."
         await self.bot.send_message(server, msg)
 
 def setup(bot):

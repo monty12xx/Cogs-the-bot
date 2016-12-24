5855481d98c0d658449fddd35c58f0a6be768172
monty12xx <montywooz@gmail.com>
2016-12-24 20:37:11 +0000

    Update serverjoin.py

--- serverjoin/serverjoin.py
+++ serverjoin/serverjoin.py
@@ -38,7 +38,7 @@ class Onjoin:
         e.timestamp = datetime.datetime.utcnow()
         avatar = self.bot.user.avatar_url if self.bot.user.avatar else self.bot.user.default_avatar_url
         e.set_image(url=avatar)
-        return self.embed_serverjoin()
+        return e
 
 def setup(bot):
     n = Onjoin(bot)

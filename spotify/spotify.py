68310bd7a2a2d4eeae0bc8bff987530959d29e64
monty12xx <montywooz@gmail.com>
2017-01-02 01:10:56 +0000

    Create spotify.py

---
+++ spotify/spotify.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import spotipy
+
+class spotify:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def spotify(self, ctx, *, content):
+        author = ctx.message.author
+        sp = spotipy.Spotify()
+        a = content
+        b = self.bot.say("how much songs?")
+        results= sp.search(q=a, limib=b)
+        for i, t in enumerate(results['tracks']['items']):
+            self.bot.say(' ', i, t['name'])
+def setup(bot):
+    n = spotify(bot)
+    bot.add_cog(n)

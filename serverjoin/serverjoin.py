a4c63d3573d5d948384f3f4146836c7428f6799c
monty12xx <montywooz@gmail.com>
2017-01-02 18:11:51 +0000

    Update serverjoin.py

--- serverjoin/serverjoin.py
+++ serverjoin/serverjoin.py
@@ -12,7 +12,7 @@ class Onjoin:
         self.bot = bot
     async def on_server_join(self, server):
         """says something when joins server"""
-        msg1 = "`-` thanks for adding me to adding me to {}\n`-`for a list of my commands do `%help`\n`-` if anything is wrong with the __bot__ :robot:  please contact me using `%contact`\n`-` for modlogs :hammer: to set them just do `%modset` for a help\n`-` if you are using modlogs please kick and ban using the `-` for trivia games `%trivia <trivia>` to start one\n`-`FeedBack And support here : http://tinyurl.com/hvuq4ah `-`\n `-` thanks for using v9 :heart:".format(server.name)
+        msg1 = "`-` thanks for adding me to adding me to {}\n`-`for a list of my commands do `%help`\n`-` if anything is wrong with the __bot__ :robot:  please contact me using `%contact`\n`-` for modlogs :hammer: to set them just do `%modset` for a help\n`-` if you are using modlogs please kick and ban using the `-` for trivia games `%trivia <trivia>` to start one\n`-`FeedBack And support here : https://discord.gg/8ftzUNw `-`\n `-` thanks for using v9 :heart:".format(server.name)
         await self.bot.send_message(server, msg1)
 
 def setup(bot):

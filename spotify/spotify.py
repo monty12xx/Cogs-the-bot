7a3831a64f88fe85a891130d7229d1b62b13c9b0
monty12xx <montywooz@gmail.com>
2017-01-04 21:54:22 +0000

    Update spotify.py

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,19 +1,38 @@
 import discord
+import time
 from discord.ext import commands
-import spotipy
+from random import choice, randint
+import cogs.utils
+import asyncio
+from cogs.utils import checks
+class disco:
+    """Disco role"""
 
-class spotify:
     def __init__(self, bot):
         self.bot = bot
+    @checks.is_owner()
+    @commands.command(pass_context = True, no_pm=True)
+    async def discorole(self, ctx, times : int,  *, role: discord.Role):
+        """only only donators !. if you want this to work contact bot owner , or donate."""
+        time = 0
+        while time < times:
+            colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+            colour = int(colour, 16)
+            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
+            time = time + 1
+            await asyncio.sleep(8.5)
+	
+    @checks.is_owner()
+    @commands.command(pass_context = True, no_pm=True)
+    async def disco(self, ctx, *, role: discord.Role):
+        """only only donators !. if you want this to work contact bot owner , or donate."""
+        while True:
+            colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+            colour = int(colour, 16)
+            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
+            await asyncio.sleep(8.5)
 
-    @commands.command(pass_context=True)
-    async def spotify(self,ctx, *, content):
-        sp = spotipy.Spotify()
-        a = content
-        b = 10
-        results= sp.search(q=a, limit=b)
-        for i in enumerate(results['tracks']['items']):
-            await self.bot.send_message(ctx.message.channel, i['name'])
+
 def setup(bot):
-    n = spotify(bot)
+    n = disco(bot)
     bot.add_cog(n)

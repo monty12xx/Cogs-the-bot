577f42f995b7e065959bd9dcd4bbaf6e48af2b3b
monty12xx <montywooz@gmail.com>
2017-01-07 13:31:54 +0000

    Update audio2.py

--- audio2/audio2.py
+++ audio2/audio2.py
@@ -1,10 +1,6 @@
 import asyncio
 import discord
-import copy
-import glob
-
 from discord.ext import commands
-from .utils.extract_info import Extract, VideoTooBig
 
 if not discord.opus.is_loaded():
     # the 'opus' library here is opus.dll on windows
@@ -14,72 +10,35 @@ if not discord.opus.is_loaded():
     # note that on windows this DLL is automatically provided for you
     discord.opus.load_opus('opus')
 
-donation_spam = {}
-
-
-INIT0 = '203649661611802624'
-
-
 class VoiceEntry:
-
     def __init__(self, message, player):
-        self.server = message.server.name
         self.requester = message.author
         self.channel = message.channel
-        self.voice_channel = message.author.voice_channel
         self.player = player
 
     def __str__(self):
-        fmt = '**{0.title}** uploaded by {0.uploader} and requested by {1.display_name}'
+        fmt = '*{0.title}* uploaded by {0.uploader} and requested by {1.display_name}'
         duration = self.player.duration
         if duration:
-            fmt += ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
+            fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
         return fmt.format(self.player, self.requester)
 
-    def embed(self):
-        data = discord.Embed(
-            color=discord.Color(value="16727871"),
-            description=self.player.webpage_url
-        )
-        duration = self.player.duration
-        data.add_field(name="Uploaded by", value=self.player.uploader)
-        data.add_field(name="Requested by", value=self.requester.display_name)
-        if duration:
-            data.add_field(name="Duration", value='{0[0]}m {0[1]}s'.format(
-                divmod(duration, 60)))
-        data.set_author(name=self.player.title, url=self.player.webpage_url)
-        data.set_thumbnail(url=self.player.thumbnail)
-        return data
-
-
 class VoiceState:
-
-    def __init__(self, bot, cog):
-        self.volume = 0.6
-        self.stop = False
+    def __init__(self, bot):
         self.current = None
         self.voice = None
         self.bot = bot
-        self.cog = cog
         self.play_next_song = asyncio.Event()
         self.songs = asyncio.Queue()
-        self.songlist = []
-        self.skip_votes = set()
+        self.skip_votes = set() # a set of user_ids that voted
         self.audio_player = self.bot.loop.create_task(self.audio_player_task())
-        self.Extract = Extract()
-
-    def votes_needed(self):
-        return round(len([i.name for i in self.voice.channel.voice_members if i.name != self.bot.user.name]) * 0.6)
 
     def is_playing(self):
         if self.voice is None or self.current is None:
             return False
 
         player = self.current.player
-        try:
-            return not player.is_done()
-        except AttributeError:
-            return False
+        return not player.is_done()
 
     @property
     def player(self):
@@ -93,80 +52,19 @@ class VoiceState:
     def toggle_next(self):
         self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
 
-    async def disconnect(self, message=False):
-        try:
-            self.player.stop()
-        except:
-            pass
-        try:
-            await self.voice.disconnect()
-        except:
-            pass
-        try:
-            self.audio_player.cancel()
-        except:
-            pass
-        try:
-            for k, v in copy.copy(self.cog.voice_states).items():
-                if v == self:
-                    del self.cog.voice_states[k]
-        except:
-            pass
-
-
-    async def create_player(self):
-        entry = self.current
-        # args = 'cache/{}.mp3'.format(entry.display_id)
-        await self.Extract.download(entry.player.webpage_url)
-        args = glob.glob('cache/{}.*'.format(entry.player.display_id))[0]
-        player = self.voice.create_ffmpeg_player(args, after=self.toggle_next)
-
-        # TODO: find a way to iterate over this using getattr and setattr
-        player.yt = entry.player.yt
-        player.title = entry.player.title
-        player.display_id = entry.player.display_id
-        player.thumbnail = entry.player.thumbnail
-        player.webpage_url = entry.player.webpage_url
-        player.download_url = entry.player.download_url
-        player.views = entry.player.views
-        player.is_live = entry.player.is_live
-        player.likes = entry.player.likes
-        player.dislikes = entry.player.dislikes
-        player.duration = entry.player.duration
-        player.uploader = entry.player.uploader
-
-        return player
-
     async def audio_player_task(self):
         while True:
-            self.current = await self.songs.get()
-            self.current.player = await self.create_player()
             self.play_next_song.clear()
-            try:
-                if not self.stop:
-                    await self.bot.send_message(self.current.channel, "Now playing")
-                    await self.bot.send_message(self.current.channel, embed=self.current.embed())
-                self.songlist.pop(0)
-            except:
-                pass
-            self.current.player.volume = self.volume
+            self.current = await self.songs.get()
+            await self.bot.send_message(self.current.channel, 'Now playing ' + str(self.current))
             self.current.player.start()
             await self.play_next_song.wait()
-            if not self.songs.empty() or len(self.voice.channel.voice_members) < 2:
-                if self.current.requester.voice_channel is not None:
-                    await self.voice.move_to(self.current.requester.voice_channel)
-                else:
-                    await self.disconnect()
-            else:
-                await self.disconnect()
-                return
-
 
 class Music:
     """Voice related commands.
+
     Works in multiple servers at once.
     """
-
     def __init__(self, bot):
         self.bot = bot
         self.voice_states = {}
@@ -174,14 +72,14 @@ class Music:
     def get_voice_state(self, server):
         state = self.voice_states.get(server.id)
         if state is None:
-            state = VoiceState(self.bot, self)
+            state = VoiceState(self.bot)
             self.voice_states[server.id] = state
 
         return state
 
     async def create_voice_client(self, channel):
-        state = self.get_voice_state(channel.server)
         voice = await self.bot.join_voice_channel(channel)
+        state = self.get_voice_state(channel.server)
         state.voice = voice
 
     def __unload(self):
@@ -193,6 +91,19 @@ class Music:
             except:
                 pass
 
+    @commands.command(pass_context=True, no_pm=True)
+    async def join(self, ctx, *, channel : discord.Channel):
+        """Joins a voice channel."""
+        try:
+            await self.create_voice_client(channel)
+        except discord.ClientException:
+            await self.bot.say('Already in a voice channel...')
+        except discord.InvalidArgument:
+            await self.bot.say('This is not a voice channel...')
+        else:
+            await self.bot.say('Ready to play audio in ' + channel.name)
+
+    @commands.command(pass_context=True, no_pm=True)
     async def summon(self, ctx):
         """Summons the bot to join your voice channel."""
         summoned_channel = ctx.message.author.voice_channel
@@ -209,80 +120,88 @@ class Music:
         return True
 
     @commands.command(pass_context=True, no_pm=True)
-    async def play(self, ctx, *, song: str):
+    async def play(self, ctx, *, song : str):
+        """Plays a song.
+
+        If there is a song currently in the queue, then it is
+        queued until the next song is done playing.
+
+        This command automatically searches as well from YouTube.
+        The list of supported sites can be found here:
+        https://rg3.github.io/youtube-dl/supportedsites.html
+        """
         state = self.get_voice_state(ctx.message.server)
-        if state.voice is None or not state.is_playing():
-            try:
-                await self.summon(ctx)
-            except Exception as e:
-                await self.bot.say('Error: {}, {}'.format(e.__class__.__name__, e))
-                raise Exception
+        opts = {
+            'default_search': 'auto',
+            'quiet': True,
+        }
+
+        if state.voice is None:
+            success = await ctx.invoke(self.summon)
+            if not success:
+                return
+
         try:
-            entry = await state.Extract.extract(song)
-            player = entry
-        except VideoTooBig:
-            await self.bot.say('video is too long, must be under 1 hour')
-            if state.voice is None or not state.is_playing():
-                await state.disconnect()
+            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
         except Exception as e:
             fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
-            await self.bot.send_message(ctx.message.channel, fmt.format(e.__class__.__name__, e))
-            if state.voice is None or not state.is_playing():
-                await state.disconnect()
+            await self.bot.send_message(ctx.message.channel, fmt.format(type(e).__name__, e))
         else:
+            player.volume = 0.6
             entry = VoiceEntry(ctx.message, player)
-            state.songlist.append(entry)
-            await state.songs.put(entry)
             await self.bot.say('Enqueued ' + str(entry))
+            await state.songs.put(entry)
 
     @commands.command(pass_context=True, no_pm=True)
-    async def volume(self, ctx, value: int):
+    async def volume(self, ctx, value : int):
         """Sets the volume of the currently playing song."""
 
-        if value > 100:
-            await self.bot.say('select a value between 0-100 pls')
-            return
         state = self.get_voice_state(ctx.message.server)
         if state.is_playing():
             player = state.player
-            player.volume = state.volume = value / 100
+            player.volume = value / 100
             await self.bot.say('Set the volume to {:.0%}'.format(player.volume))
 
     @commands.command(pass_context=True, no_pm=True)
     async def pause(self, ctx):
+        """Pauses the currently played song."""
         state = self.get_voice_state(ctx.message.server)
         if state.is_playing():
             player = state.player
             player.pause()
-        else:
-            await self.bot.say('not playing anything')
-            return
-        await self.bot.say('paused current song')
 
     @commands.command(pass_context=True, no_pm=True)
     async def resume(self, ctx):
+        """Resumes the currently played song."""
         state = self.get_voice_state(ctx.message.server)
         if state.is_playing():
             player = state.player
             player.resume()
-        else:
-            await self.bot.say('not playing anything')
-            return
-        await self.bot.say('resumed current song')
 
     @commands.command(pass_context=True, no_pm=True)
     async def stop(self, ctx):
-        server = ctx.message.server
-        state = self.get_voice_state(server) 
-        if ctx.message.author.server_permissions.mute_members == True or ctx.message.author.id == INIT0:
-            state.stop = True
-            await state.disconnect()
-            await self.bot.say('Stopped the current player')
+        """Stops playing audio and leaves the voice channel.
 
+        This also clears the queue.
+        """
+        server = ctx.message.server
+        state = self.get_voice_state(server)
+
+        if state.is_playing():
+            player = state.player
+            player.stop()
+
+        try:
+            state.audio_player.cancel()
+            del self.voice_states[server.id]
+            await state.voice.disconnect()
+        except:
+            pass
 
     @commands.command(pass_context=True, no_pm=True)
     async def skip(self, ctx):
         """Vote to skip a song. The song requester can automatically skip.
+
         3 skip votes are needed for the song to be skipped.
         """
 
@@ -292,61 +211,31 @@ class Music:
             return
 
         voter = ctx.message.author
-        if voter not in state.voice.channel.voice_members and voter.id != INIT0:
-            await self.bot.say('you are not in the current playing voice channel')
-            return
-
-        if voter == state.current.requester or voter.id == INIT0:
+        if voter == state.current.requester:
             await self.bot.say('Requester requested skipping song...')
             state.skip()
-            return
-
-        if state.current.requester.id == INIT0:
-            await self.bot.say('nah this song is good')
-            return
-
-        if voter.id not in state.skip_votes:
+        elif voter.id not in state.skip_votes:
             state.skip_votes.add(voter.id)
             total_votes = len(state.skip_votes)
-            if total_votes >= state.votes_needed():
+            if total_votes >= 3:
                 await self.bot.say('Skip vote passed, skipping song...')
                 state.skip()
             else:
-                await self.bot.say('Skip vote added, currently at {}/{}'.format(total_votes, state.votes_needed()))
+                await self.bot.say('Skip vote added, currently at [{}/3]'.format(total_votes))
         else:
             await self.bot.say('You have already voted to skip this song.')
 
     @commands.command(pass_context=True, no_pm=True)
-    async def current(self, ctx):
+    async def playing(self, ctx):
+        """Shows info about the currently played song."""
+
         state = self.get_voice_state(ctx.message.server)
         if state.current is None:
             await self.bot.say('Not playing anything.')
         else:
             skip_count = len(state.skip_votes)
-            embed = state.current.embed().add_field(
-                name="Skip count", value="{}/{}".format(skip_count, state.votes_needed()))
-            await self.bot.say(embed=embed)
-
-    @commands.command(pass_context=True)
-    async def songlist(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
-        skip_count = len(state.skip_votes)
-        data = discord.Embed(
-            color=discord.Color(value="16727871"),
-            description="Queued songs"
-        )
-        if len(state.songlist) < 1:
-            await self.bot.say("nothing is in the queue currently")
-            return
-        for i in state.songlist:
-            data.add_field(name="{}. {}".format(state.songlist.index(
-                i) + 1, i.player.title), value="Skip count: {}/{}".format(skip_count, state.votes_needed()))
-        await self.bot.say(embed=data)
-
-    @commands.command()
-    async def music(self):
-        await self.bot.say('Music commands are in %help, or %help music)
-
+            await self.bot.say('Now playing {} [skips: {}/3]'.format(state.current, skip_count))
 
 def setup(bot):
-    bot.add_cog(Music(bot))
+    n = music(bot)
+    bot.add_cog(n)
